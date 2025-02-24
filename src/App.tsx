775d8bac07d97add1af911ecb4b3f90e3bf3d9db
Asliddin <Eshkhuvvatofff@gmail.com>
2025-02-24 21:54:57 +0500

    loading fn added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import LoadingComp from './components/loadingComp/loadingComp';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 4500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      {loading ? (
+        <LoadingComp />
+      ) : (
+        <h1>hello world</h1>
+      )}
+    </>
   )
 }
 
