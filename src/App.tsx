fbe71d84b6ae7471024c5ee17d2c6900c5300053
Asliddin <Eshkhuvvatofff@gmail.com>
2025-02-25 20:11:12 +0500

    boddy style add & scrollbar style add

--- src/App.tsx
+++ src/App.tsx
@@ -35,9 +35,9 @@ const App = () => {
 
   return (
     <Router>
-      {loading ? <LoadingComp /> : 
+      {/* {loading ? <LoadingComp /> :  */}
       <MainContent />
-      }
+      {/* // } */}
     </Router>
   );
 };
