19564a947c39ceb04b723167239214edc3914c8b
Asliddin <Eshkhuvvatofff@gmail.com>
2025-03-05 20:08:33 +0500

    context menu add

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,83 @@ import Particles from './components/Backgrounds/ParticleBG/Particle';
 import Homepage from './pages/homepage/Homepage';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {
+  ContextMenu,
+  ContextMenuCheckboxItem,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu"
 
 const MainContent = () => {
   return (
-    <>
-      <Cursor />
-      <Particles />
-      <Header />
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        {/* <Route path='/login' element={<LoginPage />} /> */}
-        {/* <Route path='/register' element={<RegisterPage />} /> */}
-        {/* <Route path='/admin' element={<AdminPage />} /> */}
-        {/* <Route path='/game' element={<GamePage />} /> */}
-      </Routes>
-      <ToastContainer />
-      <Footer />
-    </>
+    <ContextMenu>
+      <ContextMenuTrigger className="w-full h-full">
+        <div className="relative w-screen h-screen">
+          <Cursor />
+          <Particles />
+          <Header />
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+          </Routes>
+          <ToastContainer />
+          <Footer />
+        </div>
+      </ContextMenuTrigger>
+      <ContextMenuContent className="w-64">
+        <ContextMenuItem inset>
+          Back
+          <ContextMenuShortcut>⌘[</ContextMenuShortcut>
+        </ContextMenuItem>
+        <ContextMenuItem inset disabled>
+          Forward
+          <ContextMenuShortcut>⌘]</ContextMenuShortcut>
+        </ContextMenuItem>
+        <ContextMenuItem inset>
+          Reload
+          <ContextMenuShortcut>⌘R</ContextMenuShortcut>
+        </ContextMenuItem>
+        <ContextMenuSub>
+          <ContextMenuSubTrigger inset>More Tools</ContextMenuSubTrigger>
+          <ContextMenuSubContent className="w-48">
+            <ContextMenuItem>
+              Save Page As...
+              <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut>
+            </ContextMenuItem>
+            <ContextMenuItem>Create Shortcut...</ContextMenuItem>
+            <ContextMenuItem>Name Window...</ContextMenuItem>
+            <ContextMenuSeparator />
+            <ContextMenuItem>Developer Tools</ContextMenuItem>
+          </ContextMenuSubContent>
+        </ContextMenuSub>
+        <ContextMenuSeparator />
+        <ContextMenuCheckboxItem checked>
+          Show Bookmarks Bar
+          <ContextMenuShortcut>⌘⇧B</ContextMenuShortcut>
+        </ContextMenuCheckboxItem>
+        <ContextMenuCheckboxItem>Show Full URLs</ContextMenuCheckboxItem>
+        <ContextMenuSeparator />
+        <ContextMenuRadioGroup value="pedro">
+          <ContextMenuLabel inset>People</ContextMenuLabel>
+          <ContextMenuSeparator />
+          <ContextMenuRadioItem value="pedro">
+            Pedro Duarte
+          </ContextMenuRadioItem>
+          <ContextMenuRadioItem value="colm">Colm Tuite</ContextMenuRadioItem>
+        </ContextMenuRadioGroup>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 };
+
 
 const App = () => {
   const [loading, setLoading] = useState(true);
